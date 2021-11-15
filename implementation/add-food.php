40752458d32823f6866a7bc047ea16e6a16d91e9
ashawe <harshsaglani@gmail.com>
2021-11-15 00:01:24 -0500

    Add add food page

---
+++ implementation/add-food.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Food Item</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/cover.css">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+
+<body class="d-flex h-100 bg-dark">
+    <!-- @ToDo: Fix "active" link in navbar -->
+    <div class="container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="mb-auto">
+            <div class="">
+                <h3 class="float-md-start mb-0 text-white">Restaurant Ordering System</h3>
+                <nav class="nav nav-masthead justify-content-center float-md-end">
+                    <a class="nav-link text-white" aria-current="page" href="#">Dashboard</a>
+                    <a class="nav-link text-white" aria-current="page" href="view-suggestions.php">Manage Ratings / Reviews</a>
+                    <ul class="navbar-nav mx-2">
+                        <li class="nav-item dropdown text-white">
+                            <a class="text-white nav-link active dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Food
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li><a class="dropdown-item text-white" href="add-food.php">Add Food</a></li>
+                                <li><a class="dropdown-item text-white" href="manage-food.php">Manage Food</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <a class="nav-link text-white" aria-current="page" href="add-chef.php">Add Chef</a>
+                </nav>
+            </div>
+        </header>
+
+        <main class="py-3 text-white">
+            <h1 class="my-5 text-center">Add Food!</h1>
+            <div class="container">
+                <div class="row" style="gap:25px">
+                    <form enctype="multipart/form-data" method="POST">
+                        <div class="mb-3">
+                            <label for="food-name" class="form-label">Food Name</label>
+                            <input type="text" class="form-control" id="food-name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="food-description" class="form-label">Description</label>
+                            <textarea class="form-control" name="food-description" id="food-description" cols="30" rows="10"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="food-img" class="form-label">Food Image</label>
+                            <input type="file" class="form-control" id="food-img" aria-describedby="img-help">
+                            <div id="img-help" class="form-text">Upload square images.</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="food-price" class="form-label">Food Price</label>
+                            <input type="number" class="form-control" id="food-price">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Add Food Item</button>
+                    </form>
+                </div>
+            </div>
+        </main>
+
+        <footer class="mt-auto text-white-50" style="position: fixed;">
+            <div class="toast-container">
+
+            </div>
+        </footer>
+    </div>
+
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
