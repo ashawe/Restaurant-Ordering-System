1a80304048cebe8629f582c1e1a16fc2be65e126
ashawe <harshsaglani@gmail.com>
2021-11-15 00:19:11 -0500

    Remove Dashboard from navbar

--- implementation/manage-food.php
+++ implementation/manage-food.php
@@ -22,7 +22,6 @@
             <div class="">
                 <h3 class="float-md-start mb-0 text-white">Restaurant Ordering System</h3>
                 <nav class="nav nav-masthead justify-content-center float-md-end">
-                    <a class="nav-link text-white" aria-current="page" href="#">Dashboard</a>
                     <a class="nav-link text-white" aria-current="page" href="view-suggestions.php">Manage Ratings / Reviews</a>
                     <ul class="navbar-nav mx-3">
                         <li class="nav-item dropdown text-white">
